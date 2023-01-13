5d34094d79f5b9b2678c4a2fce3f281fca823c74
Umar <1000slow@gmail.com>
2023-01-13 18:48:46 +0300

    axios

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,42 +4,65 @@ const form = document.querySelector('#new-post form');
 const fetchBtn = document.querySelector('#available-posts button');
 const postList = document.querySelector('ul');
 
-const url = 'https://jsonplaceholder.typicode.com/post';
+const url = 'https://jsonplaceholder.typicode.com/posts';
 
 function sendHttpRequest(method, url, type, data) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.responseType = type;
-        xhr.onload = function () {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.response)
-            } else {
-                reject(new Error('Something went wrong...'))
-            }
+    // return new Promise((resolve, reject) => {
+    //     const xhr = new XMLHttpRequest();
+    //     xhr.open(method, url);
+    //     xhr.responseType = type;
+    //     xhr.onload = function () {
+    //         if (xhr.status >= 200 && xhr.status < 300) {
+    //             resolve(xhr.response)
+    //         } else {
+    //             reject(new Error('Something went wrong...'))
+    //         }
+    //     }
+    //
+    //     xhr.onerror = function () {
+    //         reject(new Error('Failed to send request'));
+    //     }
+    //     xhr.send(JSON.stringify(data));
+    // });
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
         }
+    }).then(response => {
+        if (response.status >= 200 && response.status < 300) {
+            return response.json()
+        } else {
+            return response.json().then(errData => {
+                console.log(errData);
+                throw new Error('Something went wrong - server-side.');
+            });
+        }
+    })
+        .catch(error => {
+            console.log(error);
+            throw new Error('Something went wrong!')
+        })
 
-        xhr.onerror = function () {
-            reject(new Error('Failed to send request'));
-        }
-        xhr.send(JSON.stringify(data));
-    });
 }
 
 async function fetchPosts() {
     try {
-        const responseData = await sendHttpRequest('GET', url, 'json');
-        for (const post of responseData) {
+        const response = await axios.get(url);
+        console.log(response)
+        for (const post of response.data) {
             const postEl = document.importNode(postTemplate.content, true);
             postEl.querySelector('h2').textContent = post.title.toUpperCase();
             postEl.querySelector('p').textContent = post.body.toUpperCase();
             postEl.querySelector('li').id = post.id;
             listElement.append(postEl)
         }
-        return 'Finished'
     } catch (error) {
         alert(error.message)
+        console.log(error.response)
     }
+
 }
 
 
@@ -50,14 +73,17 @@ async function createPost(title, content) {
         body: content,
         userId: userId
     }
-    await sendHttpRequest('POST', url, 'json', post)
+    const response = await axios.post(url, post);
+    console.log(response)
 }
 
-function fetchBtnHandler() {
-    fetchPosts().then(r => console.log(r));
-}
+// function fetchBtnHandler() {
+//     fetchPosts()
+//     // .then(r => console.log(r));
+// }
 
-fetchBtn.addEventListener('click', fetchBtnHandler)
+fetchBtn.addEventListener('click', fetchPosts)
+
 form.addEventListener('submit', event => {
     event.preventDefault()
     const enteredTitle = event.currentTarget.querySelector('#title').value;
@@ -68,7 +94,7 @@ form.addEventListener('submit', event => {
 postList.addEventListener('click', event => {
     if (event.target.tagName === 'BUTTON') {
         const postId = event.target.closest('li').id;
-        sendHttpRequest('DELETE', url + `/${postId}`, 'json')
+        axios.delete(url + `/${postId}`, 'json')
     }
 })
 
